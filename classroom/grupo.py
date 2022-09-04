7bc12fabe5d04c17f209869017fe9a129409f8ca
danielvillam <55595656+danielvillam@users.noreply.github.com>
2022-09-04 13:19:14 -0500

    😁

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,6 +1,3 @@
-
-
-from classroom.asignatura import Asignatura
 from classroom.asignatura import Asignatura
 
 
@@ -8,30 +5,44 @@ class Grupo:
 
     grado = None
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
+        Grupo.asignarNombre("Grado 12")
         self._grupo = grupo
-        self._asignaturas = asignaturas
-        self.listadoAlumnos = estudiantes
+        if(estudiantes != None):
+            self.listadoAlumnos=[]
+            self.listadoAlumnos.append(estudiantes)
+        else:
+            self.listadoAlumnos = []
+        if(asignaturas != None):
+            self._asignaturas=[]
+            self._asignaturas.append(asignaturas)
+        else:
+            self._asignaturas = []
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):
         if(lista is None):
-            lista.append(alumno)
-            self.listadoAlumnos = self.listadoAlumnos + lista
+            self.listadoAlumnos.append(alumno)
         else:
-            self.listadoAlumnos = [alumno]
+            for x in lista:
+                self.listadoAlumnos.append(x)
+            self.listadoAlumnos.append(alumno)
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
         cls.grado = nombre
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
+    def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
+    def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
+
+    def __str__(self):
+        cadena="Grupo de estudiantes: "+self._grupo
+        return cadena
